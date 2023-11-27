f22ed60a1c221986d3d87199932a277b10c18b31
Trần Duy Long <21020547@vnu.edu.vn>
2023-11-28 02:15:18 +0700

    Model can read the whole dataset

--- main.py
+++ main.py
@@ -25,156 +25,166 @@ wiki_dir_path = current_dir + '/Wiki_dataset'
 wiki_dir_list = dir_list = os.listdir(wiki_dir_path)
 
 file_name_list = []
-for file in wiki_dir_list:
-    file_name_list.append(file.split('.txt')[0])
+for file_name in wiki_dir_list:
+    file_name_list.append(file_name.split('.txt')[0])
 
 # for name in file_name_list:
 
-new_txt = Coref('Wiki_dataset/Warren Buffet.txt')
+for file_name in file_name_list:
+    temp_dir = f"Extraction/{file_name}/"
+    if (os.path.exists(temp_dir) == False):
+        os.mkdir(temp_dir)
 
-print('new text: ', new_txt)
-with open('new_text.txt', 'w') as file:
-    for line in new_txt:
-        line = line.strip()
-        if (line[-1] != '.'):
-            line += '.'
-        file.write(line + '\n')
-    file.close()
+for file_name in file_name_list:
+    print('File Name: ' + file_name)
+
+    temp_dir = f"Extraction/{file_name}/"
+    
+    new_txt = Coref(f'Wiki_dataset/{file_name}.txt')
+
+    text_path = temp_dir + f"{file_name}.txt"
+    # print('new text: ', new_txt)
+    with open(text_path, 'w') as file:
+        for line in new_txt:
+            line = line.strip()
+            if (line[-1] != '.'):
+                line += '.'
+            file.write(line + '\n')
+        file.close()
 
 
-properties = {
-    'openie.affinity_probability_cap': 1/8,
-}
+    properties = {
+        'openie.affinity_probability_cap': 1/8,
+    }
 
-path = "new_text.txt"
-txt = open(path, 'r').read()
-txt = ' '.join(txt.splitlines())
+    txt = open(text_path, 'r').read()
+    txt = ' '.join(txt.splitlines())
 
-# print(txt)
+    # print(txt)
 
-with StanfordOpenIE(properties=properties) as client:
-    result = client.annotate(txt)
-    # print(result)
-    unfiltered_relation = pd.DataFrame.from_dict(result).sort_values(by=['object'])
-    # graph_image = 'graph.png'
-    # client.generate_graphviz_graph(txt, graph_image)
-    # print('Graph generated: %s.' % graph_image)
+    with StanfordOpenIE(properties=properties) as client:
+        result = client.annotate(txt)
+        # print(result)
+        unfiltered_relation = pd.DataFrame.from_dict(result).sort_values(by=['object'])
+        # graph_image = 'graph.png'
+        # client.generate_graphviz_graph(txt, graph_image)
+        # print('Graph generated: %s.' % graph_image)
 
-    print('Finished relation extraction\n')
+        print('Finished relation extraction\n')
 
-# list các câu sau khi được coref
-sentences = []
+    # list các câu sau khi được coref
+    sentences = []
 
-with open('new_text.txt', 'r') as file:
-    line = file.readline()
-    while (line != ''):
-        line = line.strip('\n')
-        print('line in new text:', line)
-        sentences.append(line)
-
+    with open(text_path, 'r') as file:
         line = file.readline()
-    
-    sentencesTokenized = [word_tokenize(sentence) for sentence in sentences]
-    # print(sentencesTokenized)
+        while (line != ''):
+            line = line.strip('\n')
+            print('line in new text:', line)
+            sentences.append(line)
 
-    file.close()
-
-tagged_sentences = TAGGER.tag_sents(sentencesTokenized)
-# print(tagged_sentences)
-
-tagged_text = ''
-for tagged_sentence in tagged_sentences:
-    tagged_words = []
-    for word, tag in tagged_sentence:
-        tagged_words.append(f"{word}_{tag}")
-    tagged_sentence_str = " ".join(tagged_words)
-    tagged_text += tagged_sentence_str + ' '
-
-tagged_text = [tagged_text]
-
-term_list1 = extractor(tagged_text, 'Noun', 8, 2, 1)
-term_list2 = extractor(tagged_text, 'AdjNoun', 8, 2, 1)
-term_list3 = extractor(tagged_text, 'AdjPrepNoun', 8, 2, 1)
-
-final_term_list = pd.concat([term_list1, term_list2, term_list3], ignore_index=True).drop_duplicates(subset='Term')
-
-ner_df = SpacyNER(sentences)
-
-ner_df = ner_df.loc[(ner_df['Label'] == 'PERSON') |
-                    (ner_df['Label'] == 'FAC') |
-                    (ner_df['Label'] == 'NORP') |
-                    (ner_df['Label'] == 'GPE') |
-                    (ner_df['Label'] == 'LOC') |
-                    (ner_df['Label'] == 'ORG')]
-
-
-terms = final_term_list['Term'].values.tolist() + ner_df['Entity'].values.tolist()
-# print(terms)
-
-filtered_rows = []
-for i in range(len(unfiltered_relation.index)):
-    row = unfiltered_relation.iloc[i]
-    for term in terms:
-        if ((term == row['subject']) and (term == row['object']) or (term == row['object'])):
-            # print(row)
-            filtered_rows.append(row)
-            break
-
-index = 0
-# while index < range(len(filtered_rows)):
-#     row = filtered_rows[index]
-#     temp_sentence = row['subject'] + ' ' + row['relation'] + ' ' + row['object']
-#     index +=
-
-filtered_rows_range = len(filtered_rows)
-i_index = 0
-while(i_index < filtered_rows_range):
-    i_row = filtered_rows[i_index]
-    i_sentence = i_row['subject'] + ' ' + i_row['relation'] + ' ' + i_row['object']
-    # print('i sentence:', i_sentence)
-    j_index = i_index + 1
-    while j_index < filtered_rows_range:
-        j_row = filtered_rows[j_index]
-        j_sentence = j_row['subject'] + ' ' + j_row['relation'] + ' ' + j_row['object']
-        # print('\tj sentence:', j_sentence)
-        if (nlp(i_sentence).similarity(nlp(j_sentence)) > 0.8):
-            del filtered_rows[j_index]
-            filtered_rows_range -= 1
-            continue
-
-        j_index += 1
-    print('Process detecting relation similarity: ', int((i_index/filtered_rows_range)*100), '%', sep='')
-    i_index += 1
+            line = file.readline()
         
-print('Process detecting relation similarity: 100%')
+        sentencesTokenized = [word_tokenize(sentence) for sentence in sentences]
+        # print(sentencesTokenized)
 
-# for row in filtered_rows:
-#     temp_sentence = row['subject'] + ' ' + row['relation'] + ' ' + row['object']
-#     if index < 10:
-#         print(temp_sentence)
-    
-#     else :
-#         break
-#     index += 1
+        file.close()
+
+    tagged_sentences = TAGGER.tag_sents(sentencesTokenized)
+    # print(tagged_sentences)
+
+    tagged_text = ''
+    for tagged_sentence in tagged_sentences:
+        tagged_words = []
+        for word, tag in tagged_sentence:
+            tagged_words.append(f"{word}_{tag}")
+        tagged_sentence_str = " ".join(tagged_words)
+        tagged_text += tagged_sentence_str + ' '
+
+    tagged_text = [tagged_text]
+
+    term_list1 = extractor(tagged_text, 'Noun', 8, 2, 1)
+    term_list2 = extractor(tagged_text, 'AdjNoun', 8, 2, 1)
+    term_list3 = extractor(tagged_text, 'AdjPrepNoun', 8, 2, 1)
+
+    final_term_list = pd.concat([term_list1, term_list2, term_list3], ignore_index=True).drop_duplicates(subset='Term')
+
+    ner_df = SpacyNER(sentences)
+
+    ner_df = ner_df.loc[(ner_df['Label'] == 'PERSON') |
+                        (ner_df['Label'] == 'FAC') |
+                        (ner_df['Label'] == 'NORP') |
+                        (ner_df['Label'] == 'GPE') |
+                        (ner_df['Label'] == 'LOC') |
+                        (ner_df['Label'] == 'ORG')]
 
 
-final_relation = pd.DataFrame(filtered_rows).sort_values(by=['object'])
+    terms = final_term_list['Term'].values.tolist() + ner_df['Entity'].values.tolist()
+    # print(terms)
 
-# for i in range(len(final_relation)):
-    
+    filtered_rows = []
+    for i in range(len(unfiltered_relation.index)):
+        row = unfiltered_relation.iloc[i]
+        for term in terms:
+            if ((term == row['subject']) and (term == row['object']) or (term == row['object'])):
+                # print(row)
+                filtered_rows.append(row)
+                break
+
+    index = 0
+    # while index < range(len(filtered_rows)):
+    #     row = filtered_rows[index]
+    #     temp_sentence = row['subject'] + ' ' + row['relation'] + ' ' + row['object']
+    #     index +=
+
+    filtered_rows_range = len(filtered_rows)
+    i_index = 0
+    while(i_index < filtered_rows_range):
+        i_row = filtered_rows[i_index]
+        i_sentence = i_row['subject'] + ' ' + i_row['relation'] + ' ' + i_row['object']
+        # print('i sentence:', i_sentence)
+        j_index = i_index + 1
+        while j_index < filtered_rows_range:
+            j_row = filtered_rows[j_index]
+            j_sentence = j_row['subject'] + ' ' + j_row['relation'] + ' ' + j_row['object']
+            # print('\tj sentence:', j_sentence)
+            if (nlp(i_sentence).similarity(nlp(j_sentence)) > 0.8):
+                del filtered_rows[j_index]
+                filtered_rows_range -= 1
+                continue
+
+            j_index += 1
+        print('Process detecting relation similarity: ', int((i_index/filtered_rows_range)*100), '%', sep='')
+        i_index += 1
+            
+    print('Process detecting relation similarity: 100%')
+
+    # for row in filtered_rows:
+    #     temp_sentence = row['subject'] + ' ' + row['relation'] + ' ' + row['object']
+    #     if index < 10:
+    #         print(temp_sentence)
+        
+    #     else :
+    #         break
+    #     index += 1
 
 
-for i in range(len(ner_df)):
-    row = ner_df.iloc[i]
-    temp = pd.DataFrame([{'subject': row['Label'], 'relation':'include', 'object':row['Entity']}])   
-    final_relation = pd.concat([temp, final_relation])
+    final_relation = pd.DataFrame(filtered_rows).sort_values(by=['object'])
+
+    # for i in range(len(final_relation)):
+        
 
 
+    # for i in range(len(ner_df)):
+    #     row = ner_df.iloc[i]
+    #     temp = pd.DataFrame([{'subject': row['Label'], 'relation':'include', 'object':row['Entity']}])   
+    #     final_relation = pd.concat([temp, final_relation])
 
-final_relation.drop_duplicates().reset_index(drop=True).to_excel('OSHUMED_SVO.xlsx')
 
-ner_df.drop_duplicates(['Entity']).reset_index(drop=True).to_excel('Ner_xlsx/Oshumed_NER.xlsx')
+    excel_path = temp_dir + file_name
+    final_relation.drop_duplicates().reset_index(drop=True).to_excel(excel_path + '_SVO.xlsx')
 
-final_term_list.reset_index(drop=True).to_excel('OSHUMED_terms.xlsx')
+    ner_df.drop_duplicates(['Entity']).reset_index(drop=True).to_excel(excel_path + '_NER.xlsx')
 
-# generator('Warren_Buffet_wiki', final_relation)
+    final_term_list.reset_index(drop=True).to_excel(excel_path + '_terms.xlsx')
+
+    generator(f'{file_name}_wiki', final_relation, ner_df, final_term_list)
